489bf29420e334759a483f850e916f0dfd7dff65
zhoushuaichang <zsc_it@163.com>
2014-12-19 08:55:51 +0800

    asdasd

---
+++ src/main/java/com/shinowit/web/InPramaController.java
@@ -0,0 +1,13 @@
+package com.shinowit.web;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * Created by Administrator on 2014/12/19.
+ */
+@RequestMapping(value="inpramam")
+public class InPramaController {
+
+    public
+
+}
